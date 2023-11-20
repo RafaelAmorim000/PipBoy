3b24c0b376fc8efe8644d3740e01fc2154918293
RafaelAmorim <113442672+RafaelAmorim000@users.noreply.github.com>
2023-11-19 23:12:34 -0300

    new Custon Layout
    ☻

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import Windows.WindowFrame;
+
+public class Main {
+
+	/**
+	 * @author Rafael Silva Amorim
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 new WindowFrame();   
+	}
+
+}
